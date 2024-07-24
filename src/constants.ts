6ce592f7579c55fbc92d189ce59aab9eacb69256
0xAlchemist <eric@ebdigital.ca>
2024-07-24 13:28:55 -0700

    working solution

--- src/constants.ts
+++ src/constants.ts
@@ -1,25 +1,77 @@
-import { Address } from "viem"
+import { Address } from "viem";
 
-const BAYC_CONTRACT: Address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-const MAYC_CONTRACT: Address = "0x60E4d786628Fea6478F785A6d7e704777c86a7c6"
-const DELEGATE_CONTRACT_ADDRESS: Address = "0x00000000000000447e69651d841bD8D104Bed493"
+const BAYC_CONTRACT: Address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
+const MAYC_CONTRACT: Address = "0x60E4d786628Fea6478F785A6d7e704777c86a7c6";
+const DELEGATE_CONTRACT_ADDRESS: Address =
+  "0x00000000000000447e69651d841bD8D104Bed493";
 
-const BAYCWALLETTEST: Address = "0x9fBDD6437840641B5dEDE244B084D31e109A0F23"
-const MAYCWALLETTEST: Address = "0xfcc2a29fe057ad15aaec24db16f382e83d3f28ed"
-const NONFTTEST: Address = "0x494651a4d47F0defE93Ad736684fA5a2F2456e52"
+const BAYCWALLETTEST: Address = "0x9fBDD6437840641B5dEDE244B084D31e109A0F23";
+const MAYCWALLETTEST: Address = "0xfcc2a29fe057ad15aaec24db16f382e83d3f28ed";
+const NONFTTEST: Address = "0x494651a4d47F0defE93Ad736684fA5a2F2456e52";
 
 export const CONTRACT_ADDRESSES = {
-    BAYC_CONTRACT,
-    MAYC_CONTRACT,
-    DELEGATE_CONTRACT_ADDRESS
+  BAYC_CONTRACT,
+  MAYC_CONTRACT,
+  DELEGATE_CONTRACT_ADDRESS,
 } as const;
 
-export const TEST_ACCOUNTS = [
-    BAYCWALLETTEST,
-    MAYCWALLETTEST,
-    NONFTTEST
-] as const;
+export const TEST_ACCOUNTS = [BAYCWALLETTEST, MAYCWALLETTEST, NONFTTEST];
 
 export const DELEGATE_V2_ABI = [
-    `function getIncomingDelegations(address to) external view returns (Delegation[] memory delegations)`
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+    ],
+    name: "getIncomingDelegations",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "enum IDelegateRegistry.DelegationType",
+            name: "type_",
+            type: "uint8",
+          },
+          {
+            internalType: "address",
+            name: "to",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "from",
+            type: "address",
+          },
+          {
+            internalType: "bytes32",
+            name: "rights",
+            type: "bytes32",
+          },
+          {
+            internalType: "address",
+            name: "contract_",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "tokenId",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct IDelegateRegistry.Delegation[]",
+        name: "delegations_",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,89 @@
 import { multicall } from "./utils";
 import { getEthersProvider } from "./ethers-provider";
-import { CONTRACT_ADDRESSES, TEST_ACCOUNTS, DELEGATE_V2_ABI } from "./constants";
+import {
+  CONTRACT_ADDRESSES,
+  TEST_ACCOUNTS,
+  DELEGATE_V2_ABI,
+} from "./constants";
 import { config } from "./wagmi";
 import { Address } from "viem";
+import { BigNumber, Bytes } from "ethers";
 
 const provider = getEthersProvider(config);
-const blockTag = 'latest';
+const blockTag = "latest";
+
+enum DelegationType {
+  NONE,
+  ALL,
+  CONTRACT,
+  ERC721,
+  ERC20,
+  ERC1155,
+}
+
+interface Delegation {
+  type_: DelegationType;
+  to: Address;
+  from: Address;
+  rights: Bytes;
+  contract_: Address;
+  tokenId: BigNumber;
+  amount: BigNumber;
+}
+
+interface DelegationStruct {
+  _: Delegation[];
+  delegations_: Delegation[];
+}
+
+interface DelegatedMapping {
+  [key: Address]: Address[];
+}
 
 try {
+  const linkedDelegations: DelegatedMapping = {};
+
+  TEST_ACCOUNTS.map((address) => {
+    linkedDelegations[address.toLowerCase() as Address] = [];
+  });
+
   const response = await multicall(
     "1",
     provider,
     DELEGATE_V2_ABI,
     TEST_ACCOUNTS.map((address: Address) => [
       CONTRACT_ADDRESSES.DELEGATE_CONTRACT_ADDRESS,
-      'getIncomingDelegations',
-      [address]
+      "getIncomingDelegations",
+      [address],
     ]),
-    { blockTag }
+    { blockTag },
   );
-  console.log(response);
-} catch(error) {
+
+  if (response.length) {
+    response.map((data: DelegationStruct) => {
+      data.delegations_.map((delegation: Delegation) => {
+        if (delegation.type_ === DelegationType.ALL) {
+          linkedDelegations[delegation.to.toLowerCase() as Address].push(
+            delegation.from,
+          );
+        } else if (delegation.type_ === DelegationType.CONTRACT) {
+          if (delegation.contract_ !== CONTRACT_ADDRESSES.BAYC_CONTRACT) return;
+          if (
+            linkedDelegations[delegation.to.toLowerCase() as Address].includes(
+              delegation.from,
+            )
+          )
+            return;
+
+          linkedDelegations[delegation.to.toLowerCase() as Address].push(
+            delegation.from,
+          );
+        }
+      });
+    });
+  }
+
+  console.log(linkedDelegations);
+} catch (error) {
   console.error(error);
 }
